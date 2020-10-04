591dff1d89f373baa66721a7cac629bf342d0aa7
Alber2001 <albertova_1989@hotmail.com>
2020-10-04 15:59:13 -0600

    Updates

---
+++ precache-manifest.f1273622391a01accfe76180e3ce525f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef24866bfd0abcb08411158538005e8",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "ae2f9eb3b3c5295d5ef8",
+    "url": "/react_deploy/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7e4d8eaed2a19b4850a8",
+    "url": "/react_deploy/static/css/main.41d0cb23.chunk.css"
+  },
+  {
+    "revision": "ae2f9eb3b3c5295d5ef8",
+    "url": "/react_deploy/static/js/2.011357a9.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/react_deploy/static/js/2.011357a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4d8eaed2a19b4850a8",
+    "url": "/react_deploy/static/js/main.b2d4febe.chunk.js"
+  },
+  {
+    "revision": "cfe8ab4211f4ac4fc66c",
+    "url": "/react_deploy/static/js/runtime-main.983cdc48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
